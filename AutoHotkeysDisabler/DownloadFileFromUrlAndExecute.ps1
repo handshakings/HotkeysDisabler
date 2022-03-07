5ab4fef3300974ff8f7342ca61355903bd333500
handshakings <60043564+handshakings@users.noreply.github.com>
2022-03-07 11:49:39 +0500

    Download file from URL & execute by Powershell

---
+++ AutoHotkeysDisabler/DownloadFileFromUrlAndExecute.ps1
@@ -0,0 +1,4 @@
+﻿$source = 'https://github.com/handshakings/HotkeysDisabler/raw/master/AutoHotkeysDisabler/bin/Debug/AutoHotkeysDisabler.exe'
+$dest = 'C:/Users\Public\AutoHotkeysDisabler.exe'
+Invoke-WebRequest -Uri $source -OutFile $dest
+Start-Process -FilePath $dest -Verb RunAs
